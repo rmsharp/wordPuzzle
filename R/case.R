44e56af753203fb88e8865113ea6f005f7c1d089
rmsharp <rmsharp@me.com>
2019-04-16 09:33:38 -0500

    Added some unit tests and enhanced case()

--- R/case.R
+++ R/case.R
@@ -3,9 +3,9 @@
 #' 
 #' @param word 1 element character vector with the word
 #' @param upper_case a logical flag indicating whether or not the word is
-#' folded to upper case or not.
+#' folded to upper case or not. Defaults to \code{TRUE}.
 #' @export
-case <- function(word, upper_case) {
+case <- function(word, upper_case = TRUE) {
   if (upper_case)
     word <- toupper(word)
   
--- tests/testthat/test_word_puzzle.R
+++ tests/testthat/test_word_puzzle.R
@@ -1,30 +1,47 @@
 library(testthat, quietly = TRUE)
 library(wordPuzzle, quietly = TRUE)
 context("word_puzzle")
+words_1 <- get_words("test_file_1.txt")
+words_2 <- get_words("test_file_2.txt")
 test_that("get_words() tokenizes puzzle description file", {
-  expect_equal(length(get_words("test_file_1.txt")), 25)
-  expect_equal(length(get_words("test_file_2.txt")), 7)
+  expect_equal(length(words_1), 25)
+  expect_equal(length(words_2), 7)
 })
 test_that("get_words() returns the correct words", {
-  expect_equal(get_words("test_file_1.txt")[[25]], '25')
-  expect_equal(get_words("test_file_1.txt")[[22]], 'twenty_two')
+  expect_equal(words_1[[25]], "25")
+  expect_equal(words_1[[22]], "twenty_two")
 })
-test_that("get_specifications() returns the correct specifications", {
-  expect_equal(get_specifications("test_file_1.txt")[['title']], "Test file 1")
-  expect_equal(get_specifications("test_file_1.txt")[['n_directions']], 20)
-  expect_equal(get_specifications("test_file_1.txt")[['width']], 25)
-  expect_equal(get_specifications("test_file_1.txt")[['left_right']], 0.3)
-  expect_equal(get_specifications("test_file_1.txt")[['right_left']], 0.2)
-  expect_equal(get_specifications("test_file_1.txt")[['up_down']], 0.2)
-  expect_equal(get_specifications("test_file_1.txt")[['down_up']], 0.2)
-  expect_equal(get_specifications("test_file_1.txt")[['down_up']], 0.2)
-  expect_equal(get_specifications("test_file_1.txt")[['up_right']], 0.1)
-  expect_equal(get_specifications("test_file_1.txt")[['up_left']], 0.1)
-  expect_equal(get_specifications("test_file_1.txt")[['down_right']], 0.1)
-  expect_equal(get_specifications("test_file_1.txt")[['down_left']], 0.1)
-})
-test_that("get_word_order() returns words in order", {
+test_that("get_word_order() returns the correct word order", {
   expect_equal(get_word_order(c("word1", "word22", "word333")), c(3, 2, 1))
+  expect_equal(get_word_order(words_1), 
+               c(23L, 24L, 21L, 22L, 17L, 18L, 15L, 14L, 19L, 13L, 16L, 20L, 
+                 11L, 12L, 8L, 7L, 3L, 5L, 4L, 9L, 1L, 6L, 10L, 2L, 25L))
+})
+specs_1 <- get_specifications("test_file_1.txt")
+test_that("get_specifications() returns the correct specifications", {
+  expect_equal(specs_1[["title"]], "Test file 1")
+  expect_equal(specs_1[["n_directions"]], 20)
+  expect_equal(specs_1[["width"]], 25)
+  expect_equal(specs_1[["height"]], 15)
+  expect_equal(specs_1[["left_right"]], 0.3)
+  expect_equal(specs_1[["right_left"]], 0.2)
+  expect_equal(specs_1[["up_down"]], 0.2)
+  expect_equal(specs_1[["down_up"]], 0.2)
+  expect_equal(specs_1[["up_right"]], 0.1)
+  expect_equal(specs_1[["up_left"]], 0.1)
+  expect_equal(specs_1[["down_right"]], 0.1)
+  expect_equal(specs_1[["down_left"]], 0.1)
+})
+test_that("form_puzzle_array creates the right object", {
+  puzzle_array <- form_puzzle_array(specs_1)
+  expect_equal(nrow(puzzle_array), 15)
+  expect_equal(ncol(puzzle_array), 25)
+  expect_true(all(puzzle_array == "."))
+})
+test_that("case() performs as expected", {
+  expect_true(case("AbcD", TRUE) == "ABCD")
+  expect_false(case("AbcD") == "AbcD")
+  expect_true(case("AbcD", FALSE) == "AbcD")
 })
 test_that("filter_out_bad_directions() returns a vector of usable directions", {
   specifications <- 
@@ -43,15 +60,15 @@ test_that("filter_out_bad_directions() returns a vector of usable directions", {
   
   directions <- c("left_right", "right_left", "up_down", "down_up", 
                   "up_right", "up_left", "down_right", "down_left")
-  filtered_directions <- filter_out_bad_directions('ape', directions, 
+  filtered_directions <- filter_out_bad_directions("ape", directions, 
                                                    specifications)
   expect_equal(filtered_directions, directions)
-  filtered_directions <- filter_out_bad_directions('longWord', directions, 
+  filtered_directions <- filter_out_bad_directions("longWord", directions, 
                                                    specifications)
   expect_equal(filtered_directions, c("up_down", "down_up"))
-  specifications['width'] <- 10
-  specifications['height'] <- 5
-  filtered_directions <- filter_out_bad_directions('longWord', directions, 
+  specifications["width"] <- 10
+  specifications["height"] <- 5
+  filtered_directions <- filter_out_bad_directions("longWord", directions, 
                                                    specifications)
   expect_equal(filtered_directions, c("left_right", "right_left"))
   })
@@ -71,9 +88,9 @@ test_that(
         down_right = 1,
         down_left = 1)
     set.seed(4567)
-    expect_equal(get_random_directions('ape', specifications), 
+    expect_equal(get_random_directions("ape", specifications), 
                  c("up_down", "down_right", "up_down", "up_right"))
-    expect_error(get_random_directions('monodelphis', specifications), 
+    expect_error(get_random_directions("monodelphis", specifications), 
                  "Width or height dimension too small for word")
     specifications['height'] <- 12
     expect_equal(get_random_directions('monodelphis', specifications), 

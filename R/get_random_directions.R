5c1a6909ef3c475646594e7c03713a09894d947e
rmsharp <rmsharp@me.com>
2019-04-16 22:01:12 -0500

    starting to setup for travis-ci

--- R/get_random_directions.R
+++ R/get_random_directions.R
@@ -4,11 +4,15 @@
 #' @param word - character vector of one element with word to be put into 
 #' puzzle array.
 #' @param specifications - puzzle specifications
+#' @importFrom stringi stri_detect_fixed
 #' @export
 get_random_directions <- function(word, specifications) {
+  directions <- attr(specifications, "names")[
+    stri_detect_fixed(attr(specifications, "names"), "right") |
+    stri_detect_fixed(attr(specifications, "names"), "left") |
+    stri_detect_fixed(attr(specifications, "names"), "down")]
   directions <- 
-    filter_out_bad_directions(word, attr(specifications, 'names')[5:12], 
-                              specifications)
+    filter_out_bad_directions(word, directions, specifications)
   directions <- 
     sample(directions, specifications[['n_directions']], replace = TRUE, 
            prob = specifications[directions])
